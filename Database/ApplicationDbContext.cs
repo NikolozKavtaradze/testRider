c1642a53f23a864c1b8579b6264f94d4762f92b8
nikoloz kavtaradze <nikoloz.kavtaradze8@gmail.com>
2024-05-24 12:10:13 +0400

    intial commit

---
+++ Database/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Entities;
+
+namespace WebApplication1.Database;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Person> Persons { get; set; }
+}
---
+++ Entities/Person.cs
@@ -0,0 +1,8 @@
+namespace WebApplication1.Entities;
+
+public class Person
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int  Age { get; set; }
+}
